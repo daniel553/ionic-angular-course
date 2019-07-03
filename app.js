dbb6c8e084de9e15e09f8ae8edfec2fab429ff1d
daniel <daniel553@github.com>
2019-07-03 13:21:07 -0500

    [Added] example courses for 03-component-basics-practice

--- app.js
+++ app.js
@@ -1,30 +1,24 @@
-const reasonInput = document.querySelector("#input-reason");
-const amountInput = document.querySelector("#input-amount");
-const btnCancel = document.querySelector("#btn-cancel");
+const courseInput = document.querySelector("#input-course");
+const ratingInput = document.querySelector("#input-rating");
 const btnAdd = document.querySelector("#btn-add");
-const expensesList = document.querySelector("#expenses-list");
-const totalExpenses = document.querySelector("#total-expenses");
+const ratingList = document.querySelector("#rating-list");
 const alertCtrl = document.querySelector('ion-alert-controller');
 let total = 0;
 
-const clear = () => {
-    console.log('Clear')
-    reasonInput.value = '';
-    amountInput.value = '';
-}
 
 btnAdd.addEventListener("click", () => {
-    const enteredReason = reasonInput.value;
-    const enteredAmount = amountInput.value;
-    console.log("Entered reason", enteredReason);
-    console.log("Entered amount", enteredAmount);
+    const enteredCourse = courseInput.value;
+    const enteredRating = ratingInput.value;
+    console.log("Entered reason", enteredCourse);
+    console.log("Entered amount", enteredRating);
 
-    if (enteredReason.trim().length <= 0 ||
-        enteredAmount.trim().length <= 0 ||
-        enteredAmount <= 0) {
+    if (enteredCourse.trim().length <= 0 ||
+        enteredRating.trim().length <= 0 ||
+        enteredRating <= 0 ||
+        enteredRating > 5) {
             alertCtrl.create({
                 header: 'Invalid inputs',
-                message: 'Please enter a valid reason and amount.',
+                message: 'Please enter a valid course name or rating [1-5].',
                 buttons: ['OK']  
             }).then(alert => {
                 alert.present();
@@ -35,15 +29,9 @@ btnAdd.addEventListener("click", () => {
     console.log("Valid input");
 
     const newItem = document.createElement('ion-item');
-    newItem.textContent = enteredReason + " $" + enteredAmount;
 
-    expensesList.appendChild(newItem);
+    newItem.innerHTML = '<b>'+enteredCourse+'</b>' + '  -  ' + enteredRating + '/5';
 
-    //Convert this string to number.
-    total += +enteredAmount;
-    totalExpenses.textContent = total;
+    ratingList.appendChild(newItem);
 
-    clear();
-})
-
-btnCancel.addEventListener('click', clear);
+});
