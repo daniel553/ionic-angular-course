bec786b9abd63e23e3c43f6c9de238c9c9c4550b
daniel <daniel553@github.com>
2019-07-03 12:46:05 -0500

    [Added] some interaction to buttons

---
+++ app.js
@@ -0,0 +1,40 @@
+const reasonInput = document.querySelector("#input-reason");
+const amountInput = document.querySelector("#input-amount");
+const btnCancel = document.querySelector("#btn-cancel");
+const btnAdd = document.querySelector("#btn-add");
+const expensesList = document.querySelector("#expenses-list");
+const totalExpenses = document.querySelector("#total-expenses");
+let total = 0;
+
+const clear = () => {
+    console.log('Clear')
+    reasonInput.value = '';
+    amountInput.value = '';
+}
+
+btnAdd.addEventListener("click", () => {
+    const enteredReason = reasonInput.value;
+    const enteredAmount = amountInput.value;
+    console.log("Entered reason", enteredReason);
+    console.log("Entered amount", enteredAmount);
+
+    if (enteredReason.trim().length <= 0 ||
+        enteredAmount.trim().length <= 0 ||
+        enteredAmount <= 0)
+        return;
+    
+    console.log("Valid input");
+
+    const newItem = document.createElement('ion-item');
+    newItem.textContent = enteredReason + " $"+enteredAmount;
+
+    expensesList.appendChild(newItem);
+
+    //Convert this string to number.
+    total += +enteredAmount;
+    totalExpenses.textContent = total;
+
+    clear();
+})
+
+btnCancel.addEventListener('click', clear);
