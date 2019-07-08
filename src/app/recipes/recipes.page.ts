442386f6f84376ff4743fe70b3408a0def12ae36
daniel <daniel553@github.com>
2019-07-08 09:51:18 -0500

    [Added] Recipes page and model

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesPage implements OnInit {
 
+  recipes: Recipe[];
+
   constructor() { }
 
   ngOnInit() {
+    this.recipes = [];
+    let n = 10;
+    while (n > 0) {
+      const recipe = new Recipe();
+      recipe.id = n.toString();
+      recipe.title = 'Recipe ' + n;
+      recipe.ingridients = ['Ing 1', 'Ing 2'];
+      recipe.imageUrl = 'https://static.fjcdn.com/pictures/Oh+hi+thar+sdfdsf+wfwsf+weewr+werwe+wer_5f6e25_3695356.jpg';
+
+      this.recipes.push(recipe);
+      n--;
+    }
   }
 
 }
