316d7e806c4704ffc160e445bf261cbb1c376447
daniel <daniel553@github.com>
2019-07-09 12:34:24 -0500

    [Added] Routing for places and missing page offers

--- src/app/places/places.module.ts
+++ src/app/places/places.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
-
+import { Routes } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
+import { PlacesRoutingModule } from './places-routing.module';
 import { PlacesPage } from './places.page';
 
 const routes: Routes = [
@@ -19,7 +19,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    PlacesRoutingModule
   ],
   declarations: [PlacesPage]
 })
