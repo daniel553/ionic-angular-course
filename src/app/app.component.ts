808d58aabd818d5a2916e0ec8e7af180fc28e739
daniel <daniel553@github.com>
2019-08-07 22:25:00 -0500

    [Added] auth guard in a simple form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
+import { AuthService } from './auth/auth.service';
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +14,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private authService: AuthService,
+    private router: Router
   ) {
     this.initializeApp();
   }
@@ -24,7 +28,8 @@ export class AppComponent {
     });
   }
 
-  onLogout(){
-    console.log('onLogout');
+  onLogout() {
+    this.authService.logout();
+    this.router.navigateByUrl('/auth');
   }
 }
