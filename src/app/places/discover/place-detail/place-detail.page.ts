d111fef9ecd6bebcadb3653a02ef3e88e4ebc0b3
daniel <daniel553@github.com>
2019-08-21 15:29:56 -0500

    [Added] action sheet controller

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,7 +1,7 @@
 import { CreateBookingComponent } from './../../../bookings/create-booking/create-booking.component';
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { NavController, ModalController } from '@ionic/angular';
+import { NavController, ModalController, ActionSheetController } from '@ionic/angular';
 import { Place } from '../../place.model';
 import { PlacesService } from '../../places.service';
 
@@ -18,7 +18,8 @@ export class PlaceDetailPage implements OnInit {
               private navCtrl: NavController,
               private route: ActivatedRoute,
               private placesService: PlacesService,
-              private modalCtrl: ModalController) { }
+              private modalCtrl: ModalController,
+              private actionSheetCtrl: ActionSheetController) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(paramMap => {
@@ -33,6 +34,36 @@ export class PlaceDetailPage implements OnInit {
   onBookPlace() {
     // this.router.navigateByUrl('places/tabs/discover');
     // this.navCtrl.navigateBack('/places/tabs/discover');
+
+    this.actionSheetCtrl.create({
+      header: 'Choose an Action',
+      buttons: [{
+        text: 'Select Date',
+        handler: () => {
+          this.openBookingModal('select');
+        }
+      }, {
+        text: 'Random Date',
+        handler: () => {
+          this.openBookingModal('random');
+        }
+      }, {
+        text: 'Cancel',
+        // role: 'destructive'// will put the button in red
+        role: 'cancel'// Only cancel as a normal button, but bottom placed.
+      }]
+    }).then(actionSheetEl => {
+      actionSheetEl.present();
+    });
+  }
+
+  /**
+   * Open modal
+   * @param mode can be used only for 2 permited strings: select or random
+   */
+  openBookingModal(mode: 'select' | 'random') {
+    console.log('MODE', mode);
+
     this.modalCtrl.create({
       component: CreateBookingComponent,
       componentProps: { selectedPlace: this.place }
@@ -44,7 +75,6 @@ export class PlaceDetailPage implements OnInit {
       if (resultData.role === 'confirm') {
         // Do confirm
       }
-
     });
   }
 
