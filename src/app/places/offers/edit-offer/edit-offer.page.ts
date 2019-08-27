39c97a75438ccc46e69096d96b2510ea58590809
daniel <daniel553@github.com>
2019-08-27 16:37:22 -0500

    [Added] dynamic form to edit offer

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
 
@@ -12,18 +13,37 @@ import { Place } from './../../place.model';
 })
 export class EditOfferPage implements OnInit {
 
+  form: FormGroup;
   place: Place;
 
   constructor(private route: ActivatedRoute, private placesService: PlacesService, private navCtrl: NavController) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(paramMap => {
-      if (!paramMap.has('placeId')){
+      if (!paramMap.has('placeId')) {
         this.navCtrl.navigateBack('/places/tabs/offers');
         return;
       }
       this.place = this.placesService.getPlace(paramMap.get('placeId'));
+      this.form = new FormGroup({
+        title: new FormControl(this.place.title, {
+          updateOn: 'blur',
+          validators: [Validators.required]
+        }),
+        description: new FormControl(this.place.description, {
+          updateOn: 'blur',
+          validators: [Validators.required, Validators.maxLength(180)]
+        })
+      });
     });
+  }
+
+  onUpdateOffer() {
+    if (!this.form.invalid) {
+      return;
+    }
+
+    console.log('Form', this.form);
   }
 
 }
