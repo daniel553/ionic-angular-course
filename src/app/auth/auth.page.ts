e11080c73a32f08a8e6c0f0014bd9ec4dac079cf
daniel <daniel553@github.com>
2019-08-27 13:04:22 -0500

    [Added] Auth for login and sign up driven form

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,7 +1,9 @@
-import { AuthService } from './auth.service';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -11,6 +13,7 @@ import { LoadingController } from '@ionic/angular';
 export class AuthPage implements OnInit {
 
   isLoading = false;
+  isLogin = true;
 
   constructor(private authService: AuthService, private router: Router, private loadingCtrl: LoadingController) { }
 
@@ -19,7 +22,7 @@ export class AuthPage implements OnInit {
 
   onLogin() {
     this.isLoading = true;
-    this.loadingCtrl.create({keyboardClose: true, message: 'Logging in...'}).then(loadingEl => {
+    this.loadingCtrl.create({ keyboardClose: true, message: 'Logging in...' }).then(loadingEl => {
       loadingEl.present();
       setTimeout(() => {
         this.isLoading = false;
@@ -28,6 +31,21 @@ export class AuthPage implements OnInit {
         this.router.navigateByUrl('/places/tabs/discover');
       }, 1500);
     });
+  }
+
+  onSubmit(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if (this.isLogin) {
+      // Server request to login
+    } else {
+      // Send to sign up
+    }
+
   }
 
 }
