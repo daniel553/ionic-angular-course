bcad5d1759ff29c8f92d96ad4f40d0e41b1c223b
daniel <daniel553@github.com>
2019-08-27 18:45:42 -0500

    [Added] create booking with Select Dates controls

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -17,12 +17,14 @@ export class PlacesService {
   private mockPlaces() {
     let i = 0;
     while (i < 15) {
-      this._places.push(new Place(
-        'P' + i,
-        'Beautiful place ' + i,
-        'A very beautiful place you can check it out for this month',
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Wollcott-house-museum-maumee-oh.jpg/1200px-Wollcott-house-museum-maumee-oh.jpg',
-        (i + 1) * 100));
+      let p = new Place('P' + i,
+      'Beautiful place' + i,
+      'A very beautiful place you can check it out for this month',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Wollcott-house-museum-maumee-oh.jpg/1200px-Wollcott-house-museum-maumee-oh.jpg',
+      100,
+      new Date('2019-01-01'),
+      new Date('2019-12-31'));
+      this._places.push(p);
       i++;
     }
   }
